1229708dd2b78fb602e563ea0e977be77fbd4d44
uyereHero <uyereemmanuel03@gmail.com>
2023-02-19 09:23:28 -0800

    betty resolved.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point (code that prints a statement)
- * Return - 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\g");
 	return (0);
 }
