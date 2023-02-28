614aeb941044eb6507d06d95ea7b5d93a9e96b2b
uyereHero <uyereemmanuel03@gmail.com>
2023-02-28 05:33:55 -0800

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	int i;
-	for (i =48; i <58; i++)
+
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (i =97; i < 103; i++)
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
