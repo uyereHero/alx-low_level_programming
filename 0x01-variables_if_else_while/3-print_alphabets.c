d4c5aab0be3d09e19bcf39a6700d9eb688544e86
uyereHero <uyereemmanuel03@gmail.com>
2023-02-28 03:39:48 -0800

    Correction of "3. alphABET"

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabet 
+ * main - Prints the alphabet
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		int i;
+
 		for (i = 0; i < 52; i++)
 		{
 			putchar(alp[i]);
